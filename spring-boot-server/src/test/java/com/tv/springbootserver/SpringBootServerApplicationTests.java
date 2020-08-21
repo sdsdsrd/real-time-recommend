caf85454efdabea98a608989cdc404993678f596
sdsdsrd <1jmss@naver.com>
2020-08-22 02:31:29 +0900

    initial commit

---
+++ spring-boot-server/src/test/java/com/tv/springbootserver/SpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tv.springbootserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
