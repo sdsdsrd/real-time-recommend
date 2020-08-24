02007b8f58d7b2d7d62cd1af7362035dfdd453e4
sdsdsrd <1jmss@naver.com>
2020-08-24 22:03:14 +0900

    테스트: 테스트 추가

--- spring-boot-server/src/test/java/com/tv/springbootserver/controller/BtvControllerTest.java
+++ spring-boot-server/src/test/java/com/tv/springbootserver/controller/BtvControllerTest.java
@@ -28,7 +28,28 @@ class BtvControllerTest {
     @DisplayName("같은 요일 같은 시간대에 가장 많이 재생한 장르 top 2 구하기 - 입력값 정상")
     @Test
     void checkGetTopGenre() throws Exception {
+        mockMvc.perform(get("/genre/{stbId}/{now}", "1", "20200828123000.237"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("같은 요일 같은 시간대에 가장 많이 재생한 contentId 구하기")
+    @Test
+    void checkGetContentId() throws Exception {
         mockMvc.perform(get("/content/{stbId}/{now}", "1", "20200828123000.237"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("contentId에 해당하는 epsdId들 구하기")
+    @Test
+    void checkGetEpsdIdsFromContentId() throws Exception {
+        mockMvc.perform(get("/epsdids/{contentId}", "{1B39EF82-210C-11DF-B973-D7B3AE21195D}"))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("vod 추천 목록 epsdId 가져오기")
+    @Test
+    void checkGetEpsdIdList() throws Exception {
+        mockMvc.perform(get("/epsd/{stbId}/{now}", "1", "20200828123000.237"))
                 .andExpect(status().isOk());
     }
 }
