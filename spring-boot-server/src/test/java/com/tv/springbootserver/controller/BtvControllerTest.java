9283b2613ef6e6fc72694f4226117f25a463a4ec
sdsdsrd <1jmss@naver.com>
2020-08-26 06:16:08 +0900

    형식: import 삭제

--- spring-boot-server/src/test/java/com/tv/springbootserver/controller/BtvControllerTest.java
+++ spring-boot-server/src/test/java/com/tv/springbootserver/controller/BtvControllerTest.java
@@ -8,7 +8,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
