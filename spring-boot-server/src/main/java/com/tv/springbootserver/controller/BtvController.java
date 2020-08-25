950361882df07a07357efa3de1d25d4fb3134811
sdsdsrd <1jmss@naver.com>
2020-08-26 06:43:22 +0900

    형식: 코드 정렬

--- spring-boot-server/src/main/java/com/tv/springbootserver/controller/BtvController.java
+++ spring-boot-server/src/main/java/com/tv/springbootserver/controller/BtvController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-
 import java.util.List;
 
 @RestController
